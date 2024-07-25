70f7a4bf1ac6616ba3a8513702dc830e531a92b2
Mohamed Ayman <79944337+RealMohamedAyman@users.noreply.github.com>
2024-07-25 10:58:44 +0300

    Create hand.ino

---
+++ hand.ino
@@ -0,0 +1,37 @@
+#include<Servo.h>
+
+Servo finger[5];
+
+void setup() {
+  Serial.begin(9600);
+
+  finger[0].attach(6);
+  finger[1].attach(5);
+  finger[2].attach(4);
+  finger[3].attach(3);
+  finger[4].attach(2);
+
+  for(int i = 0; i < 5; i++)
+  {
+    finger[i].write(180);
+    delay(500);
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) 
+  {
+    String c = Serial.readStringUntil('p');
+    for(int i = 0; i < 5; i++)
+    {
+      if (c[i+1]=='1') {
+        finger[i].write(0);
+        // delay(500);
+      } else {
+        finger[i].write(180);
+        // delay(500);
+      }
+    }
+  }
+}
+
